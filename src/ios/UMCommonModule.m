34e70c0f16e759de9729f01759fd3c6a9f23f183
xiaoxianming <xianming@bianjie.ai>
2019-07-24 10:39:49 +0800

    add device info

--- src/ios/UMCommonModule.m
+++ src/ios/UMCommonModule.m
@@ -15,6 +15,8 @@
     if ([UMConfigure respondsToSelector:sel]) {
         [UMConfigure performSelector:sel withObject:@"phonegap" withObject:@"2.0"];
     }
+    NSString *deviceID =[UMConfigure deviceIDForIntegration];
+    NSLog(@"集成测试的deviceID:%@", deviceID);
     [UMConfigure initWithAppkey:appkey channel:channel];
 }
 @end
